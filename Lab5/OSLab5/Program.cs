b2d2c45b7c8a86cd2c11dfde0de3edb80956cf8a
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-12-13 19:48:29 +0200

    lab5: add code and report

---
+++ Lab5/OSLab5/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace OSLab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+
+            int[][] AD = new int[2][];
+            AD[0] = new int[2];
+            AD[1] = new int[2];
+
+            int res = 0;
+
+            for (int j = 500000000; j > 0; j--)
+            {
+                AD[0][0]++;
+                AD[0][0]++;
+            }
+
+            AD[1][1] = AD[0][0];
+
+            Console.WriteLine(AD[0][0]);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Number of milliseconds {stopwatch.ElapsedMilliseconds}");
+
+
+
+
+
+         /*   Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            int[][] AD = new int[2][];
+            AD[0] = new int[2];
+            AD[1] = new int[2];
+
+            int res = 0;
+
+            for (int j = 500000000; j > 0; j--)
+            {
+                AD[0][0] += 2;
+            }
+
+
+            AD[1][1] = AD[0][0];
+
+
+            Console.WriteLine(AD[0][0]);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Number of milliseconds {stopwatch.ElapsedMilliseconds}");*/
+
+        }
+
+
+
+
+       
+    }
+}
