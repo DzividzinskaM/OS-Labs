91dd74f41d94b17f8bd8a6a8edc244a3dd159ec3
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-12-14 19:54:27 +0200

    lab6: add code and report

---
+++ Lab6/OSLab6/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace OSLab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n Inside main()\n");
+            string numoffunc;
+            numoffunc = Console.ReadLine();
+
+            int i = 0;
+
+          // for (; i < 0xffffff; i++) ;
+
+          
+                switch (numoffunc)
+                {
+                    case "first":
+                        FirstFunction();
+                        break;
+                    case "second":
+                        SecondFunction();
+                        break;
+
+                    case "third":
+                        ThirdFunction();
+                        break;
+                }
+           
+
+        }
+
+        void new_func1() { }
+
+        static void FirstFunction()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //process 
+                Thread.Sleep(1000);
+            }
+
+            return;
+        }
+
+        static void SecondFunction()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //process 
+                Thread.Sleep(1000);
+            }
+            return;
+        }
+
+        static void ThirdFunction()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //process 
+                Thread.Sleep(1000);
+            }
+            return;
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -6,5 +6,6 @@
 ### Lab3: [*Multilevel feedback queue scheduling*](https://github.com/DzividzinskaM/OS-Labs/tree/master/Lab3)
 ### Lab4: [*Planning*](https://github.com/DzividzinskaM/OS-Labs/tree/master/Lab4)
 ### Lab5: [*Memory optimization*](https://github.com/DzividzinskaM/OS-Labs/tree/master/Lab5)
+### Lab6: [*Profiling*](https://github.com/DzividzinskaM/OS-Labs/tree/master/Lab6)
 
 
